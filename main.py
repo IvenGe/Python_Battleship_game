681bca3ac2884c1b743059aa23ee465002dc8c9f
IvenGe <47211504+IvenGe@users.noreply.github.com>
2023-10-02 16:48:07 +0000

    bug fixes and improvment

--- main.py
+++ main.py
@@ -1,182 +1,108 @@
 import copy
 
-
-def create_board(width, board):
-    for row_index in range(width):
-        board.append([])
-        for column_index in range(width):
-            board[row_index].append(None)
-
-    return board.copy()
-
-
-def place_symbol(row, column, symbol, player, board):  # unsafe :(
-    board[player][row][column] = symbol
-
-
-def place_pawn(row, column, pawn, player, rotation, stats):
-    if check_if_pawn_place_is_valid(row, column, pawn, player, rotation, stats):
-        if rotation == '_':
-            for i in range(stats[player][pawn].get("Size")):
-                place_symbol(row + i, column, stats[player][pawn].get("Symbol"), player, play_boards)
-            stats[player][pawn]["Amount"] -= 1
-        else:
-            for i in range(stats[player][pawn].get("Size")):
-                place_symbol(row, column + i, stats[player][pawn].get("Symbol"), player, play_boards)
-            stats[player][pawn]["Amount"] -= 1
-    else:
-        print("You cant place this pawn here or you dont have that pawn left!")
-
-
-def check_if_pawn_place_is_valid(row, column, pawn, player, rotation, stats):
-    print(column + stats[player][pawn].get("Size") - WIDTH)
-    if (stats[player][pawn].get("Amount") > 0 and WIDTH > (
-            WIDTH - (row + stats[player][pawn].get("Size"))) > 0) and rotation == '_':
-
-        return not check_if_something_is_already_in_use(row, column, pawn, player, rotation, stats)
-
-    elif (stats[player][pawn].get("Amount") > 0 and WIDTH > (
-            WIDTH - (column + stats[player][pawn].get("Size"))) > 0) and rotation == '|':
-        return not check_if_something_is_already_in_use(row, column, pawn, player, rotation, stats)
-
-    else:
-        return False
-
-
-def check_if_something_is_already_in_use(row, column, pawn, player, rotation, stats):
-    if rotation == '_':
-        for i in range(stats[player][pawn].get("Size")):
-            if play_boards[player][row + i][column] is not None:
-                return True
-    if rotation == '|':
-        for i in range(stats[player][pawn].get("Size")):
-            if play_boards[player][row][column + i] is not None:
-                return True
-    return False
-
-
-def switch_turn(turn=None):
-    if turn == 1:
-        turn = 2
-    else:
-        turn = 1
-    return turn
-
-
-def get_all_pawns_in_text(pawns):
-    text = ""
-    for pawn in pawns.keys():
-        text += str(pawns[pawn].get("Amount")) + " " + pawn + "'s from size " + str(pawns[pawn].get("Size")) + ', '
-
-    return text
-
-
-def print_board(board):
-    text = ""
-    for counter in range(len(board)):
-        for index in range(len(board)):
-            if board[index][counter] is not None:
-                text += str(board[index][counter] + " ")
-            else:
-                text += ". "
-        text += '\n'
-    return text
-
-
-def guess(row, column, player_who_take_guess, board):
-    if board[switch_turn(player_who_take_guess)][row][column] is not None:
-        place_symbol(row, column, '*', switch_turn(player_who_take_guess), play_boards)
-        place_symbol(row, column, '*', player_who_take_guess, guess_boards)
-        print("HIT")
-    else:
-        place_symbol(row, column, '-', switch_turn(player_who_take_guess), play_boards)
-        place_symbol(row, column, '-', player_who_take_guess, guess_boards)
-        print("MISS")
-
-
-def check_for_winner():
-    total = 0
-
-    for player in range(len(stats)):
-        for index in stats[player]:
-            total += stats[player][index].get("Amount") * stats[player][index].get("Size")
-        if total == get_total():
-            return player
-    return 0
-
-
-def get_total():
-    total = 0
-    for player in range(len(pawns)):
-        for index in stats[player]:
-            total += stats[player][index].get("Amount") * stats[player][index].get("Size")
-    return total
-
-
-# board settings
 WIDTH = 10
 
 pawns = {
-    "vliegdekschip": {
-        "Symbol": 'v',
-        "Amount": 1,
-        "Size": 6
-    },
-    "slagschip": {
-        "Symbol": 's',
-        "Amount": 2,
-        "Size": 4
-    },
-    "onderzeeër": {
-        "Symbol": 'o',
-        "Amount": 3,
-        "Size": 3
-    }, "Patrouilleschip": {
-        "Symbol": 'p',
-        "Amount": 4,
-        "Size": 2
-    },
-
+    "vliegdekschip": {"Symbol": 'v', "Amount": 1, "Size": 6},
+    "slagschip": {"Symbol": 's', "Amount": 2, "Size": 4},
+    "onderzeeër": {"Symbol": 'o', "Amount": 3, "Size": 3},
+    "Patrouilleschip": {"Symbol": 'p', "Amount": 4, "Size": 2},
 }
 
-stats = [None, copy.deepcopy(pawns),
-         copy.deepcopy(pawns)]  # Amount is now how much they have left & player 0 do not exist
-play_boards = [None, create_board(WIDTH, []), create_board(WIDTH, [])]  # player one and 2
-guess_boards = [None, create_board(WIDTH, []), create_board(WIDTH, [])]
+stats = [None, copy.deepcopy(pawns), copy.deepcopy(pawns)]
 
-# Variables
-winner = 0
+
+def create_board(width):
+    return [[None for _ in range(width)] for _ in range(width)]
+
+
+def print_board(board):
+    text = "  " + " ".join(str(i) for i in range(len(board))) + "\n"
+    for idx, row in enumerate(board):
+        row_text = str(idx) + " " + " ".join([cell if cell else '.' for cell in row])
+        text += row_text + "\n"
+    return text
+
+
+def place_symbol(board, row, column, symbol):
+    board[row][column] = symbol
+
+
+def switch_turn(turn):
+    return 1 if turn == 2 else 2
+
+
+def is_position_valid_for_placement(board, row, column, pawn, rotation):
+    size = pawn["Size"]
+    if rotation == "_":
+        if column + size > WIDTH:
+            return False
+        return all([board[row][column + i] is None for i in range(size)])
+    else:
+        if row + size > WIDTH:
+            return False
+        return all([board[row + i][column] is None for i in range(size)])
+
+
+def place_pawn(board, row, column, pawn, rotation):
+    size = pawn["Size"]
+    symbol = pawn["Symbol"]
+    for i in range(size):
+        r, c = (row + i, column) if rotation == "|" else (row, column + i)
+        place_symbol(board, r, c, symbol)
+
+
+def total_occupied_cells():
+    total = 0
+    for player in stats:
+        if player:
+            for _, details in player.items():
+                total += details["Amount"] * details["Size"]
+    return total
+
+
+def check_for_winner(board):
+    return sum(1 for row in board for cell in row if cell == '*') == total_occupied_cells()
+
+
+play_boards = [None, create_board(WIDTH), create_board(WIDTH)]
+guess_boards = [None, create_board(WIDTH), create_board(WIDTH)]
 turn = 1
+winner = None
 
-# setup
+# Setup Phase
+for player in [1, 2]:
+    while True:
+        print("Player", player, "setup phase")
+        print(print_board(play_boards[player]))
+        pawn_name = input("Which boat: " + ", ".join([f"{name}({details['Amount']} left)" for name, details in stats[player].items() if details['Amount'] > 0]) + ": ")
+        if pawn_name not in stats[player] or stats[player][pawn_name]["Amount"] <= 0:
+            print("Invalid choice!")
+            continue
+        row, column = map(int, input("Enter row, column: ").split(","))
+        rotation = input("In which direction (_ or |): ")
+        if is_position_valid_for_placement(play_boards[player], row, column, stats[player][pawn_name], rotation):
+            place_pawn(play_boards[player], row, column, stats[player][pawn_name], rotation)
+            stats[player][pawn_name]["Amount"] -= 1
+        else:
+            print("Invalid placement!")
+        if all([details["Amount"] == 0 for _, details in stats[player].items()]):
+            break
 
-is_player_setup_done = [None, None, None]  # index 1 is player 1
-
-while is_player_setup_done[turn] is None:
-    print("Its player " + str(turn) + " turn to place pawns\n")
-    place_pawn(int(input("On which row\n")), int(input("On which column\n")),
-               str(input("Witch boat: " + get_all_pawns_in_text(stats[turn]) + "\n")), turn,
-               str(input("In which direction: ( _ / | )\n")), stats)
-
-    print("Are you done ? Type yes ")
-    print(print_board(play_boards[turn]))
-
-    if str(input()).lower().find('e') >= 0:
-        is_player_setup_done[turn] = True
-        switch_turn(turn)
-        print("Switching sides")
-        turn = switch_turn(turn)
-
-# running
-
-while winner == 0:
-    print("Its player " + str(turn) + " turn to place guess\n")
+# Game Phase
+while not winner:
+    print("Player", turn, "guess phase")
     print(print_board(guess_boards[turn]))
-    print("-" * WIDTH)
-    print(print_board(play_boards[turn]))
-    guess(int(input("On which row\n")), int(input("On which column\n")), turn, play_boards)
-    print(print_board(guess_boards[turn]))
+    row, column = map(int, input("Enter row, column to guess: ").split(","))
+    if play_boards[switch_turn(turn)][row][column]:
+        print("HIT!")
+        place_symbol(guess_boards[turn], row, column, '*')
+        place_symbol(play_boards[switch_turn(turn)], row, column, '*')
+    else:
+        print("MISS!")
+        place_symbol(guess_boards[turn], row, column, '-')
+    if check_for_winner(guess_boards[turn]):
+        winner = turn
     turn = switch_turn(turn)
-    winner = check_for_winner()
 
-print("Winner is: player" + str(winner))
+print("Winner is: Player", winner)
